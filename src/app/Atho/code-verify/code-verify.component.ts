43bc73290945bf789c1bab4b66f643df125d8735
ahmed12v <ahnurmed16@gmail.com>
2025-04-26 17:52:37 +0300

    athouFinsh

--- src/app/Atho/code-verify/code-verify.component.ts
+++ src/app/Atho/code-verify/code-verify.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-code-verify',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './code-verify.component.html',
   styleUrl: './code-verify.component.css'
 })
